62fedf14b6302fb45ea14ba9b57e885ed495f4d4
PROG.ANES <118551011+DRANESSS@users.noreply.github.com>
2022-12-22 18:49:18 +0300

    Update tepthon.sh

--- tepthon.sh
+++ tepthon.sh
@@ -1,6 +1,6 @@
 branch=main
-git clone -b $branch https://github.com/anes10a/tepthon /root/anes10a
-cp tepthon/.env /root/anes10a/.env
+git clone -b $branch https://github.com/anes10a/quran /root/anes10a
+cp quran/.env /root/anes10a/.env
 cd /root/anes10a
-docker build . --rm --force-rm --compress --pull --file Dockerfile -t tepthon
-docker run --privileged --env-file .env --rm -i tepthon
+docker build . --rm --force-rm --compress --pull --file Dockerfile -t quran
+docker run --privileged --env-file .env --rm -i quran
