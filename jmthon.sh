5cac2fcf7f1a744cc6b18a65ceccebc39d83edc0
Anes10a <118900519+Anes10a@users.noreply.github.com>
2022-12-22 15:27:06 +0300

    Update jmthon.sh

--- jmthon.sh
+++ jmthon.sh
@@ -1,6 +1,6 @@
-branch=master
-git clone -b $branch https://github.com/thejmthon/sbb_b0 /root/thejmthon
-cp sbb_b0/.env /root/thejmthon/.env
-cd /root/thejmthon
-docker build . --rm --force-rm --compress --pull --file Dockerfile -t sbb_b0
-docker run --privileged --env-file .env --rm -i sbb_b0
+branch=main
+git clone -b $branch https://github.com/anes10a/tepthon /root/anes10a
+cp tepthon/.env /root/anes10a/.env
+cd /root/anes10a
+docker build . --rm --force-rm --compress --pull --file Dockerfile -t tepthon
+docker run --privileged --env-file .env --rm -i tepthon
