165ad92e95f2b2949cd0e814424f7cecc1b2d97a
Source Tepthon <121296616+TheTepthon@users.noreply.github.com>
2022-12-23 16:25:26 +0300

    Update tepthon.sh

--- tepthon.sh
+++ tepthon.sh
@@ -1,6 +1,6 @@
 branch=master
-git clone -b $branch https://github.com/anes10a/quran /root/anes10a
-cp quran/.env /root/anes10a/.env
-cd /root/anes10a
+git clone -b $branch https://github.com/anes10a/quran /root/TheTepthon
+cp quran/.env /root/TheTepthon/.env
+cd /root/TheTepthon
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t quran
 docker run --privileged --env-file .env --rm -i quran
